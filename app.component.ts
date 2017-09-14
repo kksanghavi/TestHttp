15bca8d038265a8830eea3eb0a7b9c653306d744
kksanghavi <kksanghavi@gmail.com>
2017-09-14 18:48:30 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import {HTTPService} from './HTTPService';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  providers: [HTTPService]
+})
+export class AppComponent {
+  _httpService: HTTPService;
+  title = 'My First Angular App ';
+
+  constructor(httpService: HTTPService){
+    this._httpService = httpService;
+  }
+
+  OnClick(){
+    //alert("OnClick --- START");
+
+    this._httpService.ExecuteRequest();
+
+    //alert("OnClick --- END");
+  }
+}
